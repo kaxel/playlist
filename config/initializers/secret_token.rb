1fe0cee4727610f4d46ef635dbd0794f730dadce
kaxel <ka@ashland.io>
2018-04-17 13:32:32 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Songlistr::Application.config.secret_token = 'ffa119d982b7814da4497a4f1ff36b90695df3e1ca72e3941696dd106fefe6236b06421b93f3bf6c0e64cfc9bc0d0e518289c0b468d10674b29298e4c3d79c30'
