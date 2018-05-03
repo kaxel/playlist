dbcba006f365d9a03fd80793a792cea0c951ed57
kaxel <ka@ashland.io>
2018-05-03 15:30:41 -0700

    clean up old widgets footrpint

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -1,10 +1,10 @@
-class WidgetsController < ApplicationController
-  before_action :set_widget, only: [:show, :edit, :update, :destroy]
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
-  # GET /widgets
-  # GET /widgets.json
+  # GET /users
+  # GET /users.json
   def index
-    @widgets = Widget.all
+    @users = User.all
   end
 
   # GET /widgets/1
@@ -14,61 +14,61 @@ class WidgetsController < ApplicationController
 
   # GET /widgets/new
   def new
-    @widget = Widget.new
+    @user = User.new
   end
 
   # GET /widgets/1/edit
   def edit
   end
 
-  # POST /widgets
-  # POST /widgets.json
+  # POST /users
+  # POST /users.json
   def create
-    @widget = Widget.new(widget_params)
+    @user = User.new(user_params)
 
     respond_to do |format|
-      if @widget.save
-        format.html { redirect_to @widget, notice: 'Widget was successfully created.' }
-        format.json { render :show, status: :created, location: @widget }
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
-        format.json { render json: @widget.errors, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /widgets/1
-  # PATCH/PUT /widgets/1.json
+  # PATCH/PUT /users/1
+  # PATCH/PUT /users/1.json
   def update
     respond_to do |format|
-      if @widget.update(widget_params)
-        format.html { redirect_to @widget, notice: 'Widget was successfully updated.' }
-        format.json { render :show, status: :ok, location: @widget }
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit }
-        format.json { render json: @widget.errors, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /widgets/1
-  # DELETE /widgets/1.json
+  # DELETE /users/1
+  # DELETE /users/1.json
   def destroy
-    @widget.destroy
+    @user.destroy
     respond_to do |format|
-      format.html { redirect_to widgets_url, notice: 'Widget was successfully destroyed.' }
+      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_widget
-      @widget = Widget.find(params[:id])
+    def set_user
+      @user = User.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def widget_params
-      params.require(:widget).permit(:name, :description, :stock)
+    def user_params
+      params.require(:user).permit(:email, :description)
     end
 end
